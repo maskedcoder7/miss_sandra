0ca1826e3ba1c24c19d0abc2151fa5bf7805e17c
theshubhamgundu <80262881+theshubhamgundu@users.noreply.github.com>
2021-06-24 02:29:43 +0530

    Update sample_config.py

--- Sandrabot/sample_config.py
+++ Sandrabot/sample_config.py
@@ -4,7 +4,7 @@ import os
 
 
 def get_user_list(config, key):
-    with open("{}/MashaRoBot/{}".format(os.getcwd(), config), "r") as json_file:
+    with open("{}/Sandrabot/{}".format(os.getcwd(), config), "r") as json_file:
         return json.load(json_file)[key]
 
 
@@ -18,19 +18,19 @@ class Config(object):
     API_HASH = "awoo"
     TOKEN = "BOT_TOKEN"  # This var used to be API_KEY but it is now TOKEN, adjust accordingly.
     OWNER_ID = 792109647  # If you dont know, run the bot and do /id in your private chat with it, also an integer
-    OWNER_USERNAME = "Sawada"
-    SUPPORT_CHAT = "OnePunchSupport"  # Your own group for support, do not add the @
+    OWNER_USERNAME = "theshubhamgundu"
+    SUPPORT_CHAT = "blackstormfederation"  # Your own group for support, do not add the @
     JOIN_LOGGER = (
-        -1001253661229
+        -1001434684100
     )  # Prints any new group the bot is added to, prints just the name and ID.
     EVENT_LOGS = (
-        -1001190806654
+        -1001434684100
     )  # Prints information like gbans, sudo promotes, AI enabled disable states that may help in debugging and shit
 
     # RECOMMENDED
     SQLALCHEMY_DATABASE_URI = "something://somewhat:user@hosturl:port/databasename"  # needed for any database modules
     LOAD = []
-    NO_LOAD = ["rss", "cleaner", "connection", "math"]
+    NO_LOAD = ["rss", "cleaner", "connection"]
     WEBHOOK = False
     INFOPIC = True
     URL = None
