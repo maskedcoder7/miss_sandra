98619a7e9698aa8598ac6be16d640574e16f60aa
maskedcoder <85559138+maskedcoder7@users.noreply.github.com>
2021-06-26 20:07:57 +0530

    Update __init__.py
    # Load at end to ensure all prev variables have been set
    from Sandrabot.Modules.helper_funcs.handlers import (
        CustomCommandHandler,
        CustomMessageHandler,
        CustomRegexHandler,
    )
    
    # make sure the regex handler can take extra kwargs
    tg.RegexHandler = CustomRegexHandler
    tg.CommandHandler = CustomCommandHandler
    tg.MessageHandler = CustomMessageHandler  #fisfis

--- Sandrabot/__init__.py
+++ Sandrabot/__init__.py
@@ -203,14 +203,4 @@ WOLVES = list(WOLVES)
 DEMONS = list(DEMONS)
 TIGERS = list(TIGERS)
 
-# Load at end to ensure all prev variables have been set
-from Sandrabot.Modules.helper_funcs.handlers import (
-    CustomCommandHandler,
-    CustomMessageHandler,
-    CustomRegexHandler,
-)
-
-# make sure the regex handler can take extra kwargs
-tg.RegexHandler = CustomRegexHandler
-tg.CommandHandler = CustomCommandHandler
-tg.MessageHandler = CustomMessageHandler
+ 
